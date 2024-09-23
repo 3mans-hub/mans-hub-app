aa64239e2c6ad9bb52d9e2227b06ba230c6828e6
hgb926 <hgb926@naver.com>
2024-09-23 13:51:22 +0900

    SideBar.js 액티브 클래스 부여

--- src/layout/SideBar.js
+++ src/layout/SideBar.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './styles/SideBar.module.scss';
-import {PiPlus} from "react-icons/pi";
+import { PiPlus } from "react-icons/pi";
 
 const Sidebar = () => {
+    // 클릭된 그룹의 인덱스를 저장하는 state
+    const [active, setActive] = useState(null);
+
+    const groups = ['그룹1', '그룹2'];
+
+    // 클릭 이벤트 핸들러 (클릭된 그룹의 인덱스를 저장)
+    const handleGroupClick = (index) => {
+        setActive(index);
+    };
+
     return (
         <div className={styles.sidebar}>
-            <div className={styles.mainIcon}>로고</div>
+            <div className={`${styles.mainIcon} ${active === null ? styles.active : ''}`}>로고</div>
             <div className={styles.line}></div>
-            <div className={styles.icon}>그룹1</div>
-            <div className={styles.icon}>그룹2</div>
-            <div className={styles.addGroup}><PiPlus/></div>
+
+            {groups.map((group, index) => (
+                <div
+                    key={index}
+                    className={`${styles.icon} ${active === index ? styles.active : ''}`}
+                    onClick={() => handleGroupClick(index)}
+                >
+                    {group}
+                </div>
+            ))}
+
+            <div className={styles.addGroup}>
+                <PiPlus />
+            </div>
         </div>
     );
 };
