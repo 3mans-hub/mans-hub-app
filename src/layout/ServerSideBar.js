365902a434ef76b9002edde8fb7a8cbaaf859a81
hgb926 <hgb926@naver.com>
2024-09-23 21:58:50 +0900

    Userstatus 파일 구조 리팩터링 (components로 옮김)

--- src/layout/ServerSideBar.js
+++ src/layout/ServerSideBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './styles/ServerSideBar.module.scss';
-import UserStatus from './UserStatus'
+import UserStatus from '../components/UserStatus'
 import {useDispatch, useSelector} from "react-redux";
 
 const ServerSidebar = () => {
@@ -11,13 +11,13 @@ const ServerSidebar = () => {
 
     return (
         <div className={styles.serverSidebar}>
-            <p className={styles.serverName}>서버 이름</p>
-            {/*
-                여기에 서버 관련 내용은 다른 디렉터리에서
-                컴포넌트화 하고 넣으면 좋을 것 같습니다 - 기범
-             */}
 
-            <UserStatus />
+            {/*<p className={styles.serverName}>서버 이름</p>*/}
+            {/*/!**/}
+            {/*    여기에 서버 관련 내용은 다른 디렉터리에서*/}
+            {/*    컴포넌트화 하고 넣으면 좋을 것 같습니다 - 기범*/}
+            {/* *!/*/}
+            {/*<UserStatus />*/}
         </div>
     );
 };
