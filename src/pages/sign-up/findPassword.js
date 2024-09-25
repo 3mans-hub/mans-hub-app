7a11cddc6a831008df139245f8a7d4e4b2e14957
JinWooP98 <qwdk0406@naver.com>
2024-09-25 22:26:36 +0900

    비밀번호 찾기 페이지 생성

--- src/pages/sign-up/findPassword.js
+++ src/pages/sign-up/findPassword.js
@@ -1,14 +1,53 @@
-import React from 'react';
-import styles from "./SignIn.module.scss";
+import React, {useEffect, useState} from 'react';
+import styles from "./findPassword.module.scss";
 import SignUpInput from "../../components/SignUpInput";
+import SignUpBtn from "../../components/SignUpBtn";
 
 
 const FindPassword = () => {
+
+    const [email, setEmail] = useState("");
+
+    const [verificationCode, setVerificationCode] = useState("");
+
+    const [newPassword, setNewPassword] = useState("");
+
+    const [confirmPassword, setConfirmPassword] = useState("");
+
+    const [isEmail, setIsEmail] = useState(false);
+
+    const [step, setStep] = useState(1);
+
+    const validateEmail = (email) => {
+        // 이메일 형식을 확인하는 정규식
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+    };
+
+    useEffect(() => {
+        setIsEmail(validateEmail(email))
+    }, [email]);
+
+    const nextStep = () => {
+        setStep(step + 1);
+    }
+
     return (
         <div>
             <div className={styles.container}>
-                <h1 className={styles.title}>비밀번호 찾기</h1>
-                <SignUpInput type={'text'} content={'이메일'}/>
+                {(step === 1 || step === 2 ) && <h1 className={styles.title}>비밀번호 찾기</h1> }
+                {(step >= 3) && <h1 className={styles.title}>비밀번호 변경</h1> }
+                {(step === 1 || step === 2 ) && <SignUpInput type={'text'} content={'이메일'} setValue={setEmail} disable={step !== 1}/> }
+                {( step === 2 ) && <SignUpInput type={'text'} content={'인증번호'} setValue={setVerificationCode}/> }
+
+                {step === 1 && <SignUpBtn type={isEmail} content={'인증번호 요청'} eventHandler={nextStep}/>}
+                {step === 2 && <SignUpBtn type={verificationCode !== ""} content={'인증번호 인증'} eventHandler={nextStep}/>}
+
+                {( step >= 3 ) && <SignUpInput type={'password'} content={'비밀번호'} setValue={setNewPassword}/> }
+                {( step === 4 ) && <SignUpInput type={'password'} content={'비밀번호 확인'} setValue={setConfirmPassword}/> }
+
+                {step === 3 && <SignUpBtn type={newPassword !== ""} content={'다음'} eventHandler={nextStep}/>}
+                {step === 4 && <SignUpBtn type={confirmPassword === newPassword} content={'비밀번호 변경'}/>}
             </div>
         </div>
     );
