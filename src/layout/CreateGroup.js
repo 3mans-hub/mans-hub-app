f824c5e46cc51b6104136f5c3321f6356565ba11
JinWooP98 <qwdk0406@naver.com>
2024-10-28 22:57:35 +0900

    createGroup 생성중

--- src/layout/CreateGroup.js
+++ src/layout/CreateGroup.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import SignUpBtn from "../components/SignUpBtn";
 
 
 const CreateGroup = () => {
 
-    const userData = sessionStorage.getItem("userData")
+    const userData = JSON.parse(sessionStorage.getItem("userData"));
+
+
     return (
-        <div>
-            <input type="text" placeholder={userData.nickName}/>
-            <SignUpBtn  />
+        <div >
+            <input type="text" placeholder={userData.nickname + "님의 서버"}/>
+            <button>그룹 만들기 </button>
         </div>
     );
 };
